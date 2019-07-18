c1f6e16ed9683048b4f7c9d9ea0e6b4fc24e377c
qxx <xiaoxu@mgtv.xom>
2019-07-18 16:35:41 +0800

    还原为原始的版本

--- decode.py
+++ decode.py
@@ -2,16 +2,10 @@
 import cv2
 import numpy as np
 import random
-import os, os.path
+import os
 from argparse import ArgumentParser
 ALPHA = 5
 
-
-def file_extension(path): 
-    pathes=os.path.splitext(path)
-    if len(pathes) > 1:
-        return pathes[1]
-    return "txt"
 
 def build_parser():
     parser = ArgumentParser()
@@ -19,7 +13,6 @@ def build_parser():
     parser.add_argument('--image', dest='img', required=True)
     parser.add_argument('--result', dest='res', required=True)
     parser.add_argument('--alpha', dest='alpha', default=ALPHA)
-    parser.add_argument('--compress', dest='compress', default=False)
     return parser
 
 
@@ -29,17 +22,15 @@ def main():
     ori = options.ori
     img = options.img
     res = options.res
-    compress = options.compress
-
     alpha = float(options.alpha)
     if not os.path.isfile(ori):
         parser.error("original image %s does not exist." % ori)
     if not os.path.isfile(img):
         parser.error("image %s does not exist." % img)
-    decode(ori, img, res, alpha, compress)
+    decode(ori, img, res, alpha)
 
 
-def decode(ori_path, img_path, res_path, alpha, compress):
+def decode(ori_path, img_path, res_path, alpha):
     ori = cv2.imread(ori_path)
     img = cv2.imread(img_path)
     ori_f = np.fft.fft2(ori)
@@ -56,23 +47,8 @@ def decode(ori_path, img_path, res_path, alpha, compress):
     for i in range(int(height / 2)):
         for j in range(width):
             res[x[i]][y[j]] = watermark[i][j]
-    suffix = file_extension(res_path)
-    if suffix == '.jpg1' or suffix == '.jpeg1':
-        # https://docs.opencv.org/master/d4/da8/group__imgcodecs.html
-        if compress:
-            cv2.imwrite(res_path, res, [int(cv2.IMWRITE_JPEG_OPTIMIZE), 1])
-        else:
-            cv2.imwrite(res_path, res, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-    elif  suffix == '.png1':
-        if compress:
-            # For PNG, it can be the compression level from 0 to 9. A higher value means a smaller size and longer compression time. 
-            cv2.imwrite(res_path, res, [int(cv2.IMWRITE_PNG_COMPRESSION), 5])
-        else:
-            # https://docs.opencv.org/master/d4/da8/group__imgcodecs.html#gaa60044d347ffd187161b5ec9ea2ef2f9
-            cv2.imwrite(res_path, res, [int(cv2.IMWRITE_PNG_STRATEGY), cv2.IMWRITE_PNG_STRATEGY_DEFAULT ])
-    else:
-        print("WRONG OUTPUT TYPE", suffix)
-        cv2.imwrite(res_path, res, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    cv2.imwrite(res_path, res, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
 
 if __name__ == '__main__':
     main()
--- encode.py
+++ encode.py
@@ -2,16 +2,10 @@
 import cv2
 import numpy as np
 import random
-import os, os.path
+import os
 from argparse import ArgumentParser
 ALPHA = 5
 
-
-def file_extension(path): 
-    pathes=os.path.splitext(path)
-    if len(pathes) > 1:
-        return pathes[1]
-    return "null"
 
 def build_parser():
     parser = ArgumentParser()
@@ -19,7 +13,6 @@ def build_parser():
     parser.add_argument('--watermark', dest='wm', required=True)
     parser.add_argument('--result', dest='res', required=True)
     parser.add_argument('--alpha', dest='alpha', default=ALPHA)
-    parser.add_argument('--compress', dest='compress', default=False)
     return parser
 
 
@@ -30,29 +23,25 @@ def main():
     wm = options.wm
     res = options.res
     alpha = float(options.alpha)
-    compress = options.compress
-
     if not os.path.isfile(img):
         parser.error("image %s does not exist." % img)
     if not os.path.isfile(wm):
         parser.error("watermark %s does not exist." % wm)
-    encode(img, wm, res, alpha, compress)
+    encode(img, wm, res, alpha)
 
 
-def encode(img_path, wm_path, res_path, alpha, compress):
+def encode(img_path, wm_path, res_path, alpha):
     img = cv2.imread(img_path)
     img_f = np.fft.fft2(img)
     height, width, channel = np.shape(img)
     watermark = cv2.imread(wm_path)
     wm_height, wm_width = watermark.shape[0], watermark.shape[1]
-    x, y = list(range(int(height/3))), list(range(width))
-    # print(x)
-    # sys.exit(0)
+    x, y = list(range(int(height / 2))), list(range(width))
     random.seed(height + width)
     random.shuffle(x)
     random.shuffle(y)
     tmp = np.zeros(img.shape)
-    for i in range(int(height / 3)):
+    for i in range(int(height / 2)):
         for j in range(width):
             if x[i] < wm_height and y[j] < wm_width:
                 tmp[i][j] = watermark[x[i]][y[j]]
@@ -60,27 +49,8 @@ def encode(img_path, wm_path, res_path, alpha, compress):
     res_f = img_f + alpha * tmp
     res = np.fft.ifft2(res_f)
     res = np.real(res)
-    
-    # cv2.imwrite(res_path, res, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-
-    suffix = file_extension(res_path)
-    if suffix == '.jpg' or suffix == '.jpeg':
-        # https://docs.opencv.org/master/d4/da8/group__imgcodecs.html
-        if compress:
-            cv2.imwrite(res_path, res, [int(cv2.IMWRITE_JPEG_OPTIMIZE), 1])
-        else:
-            cv2.imwrite(res_path, res, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
-    elif  suffix == '.png':
-        if compress:
-            # For PNG, it can be the compression level from 0 to 9. A higher value means a smaller size and longer compression time. 
-            cv2.imwrite(res_path, res, [int(cv2.IMWRITE_PNG_COMPRESSION), 5])
-        else:
-            # https://docs.opencv.org/master/d4/da8/group__imgcodecs.html#gaa60044d347ffd187161b5ec9ea2ef2f9
-            cv2.imwrite(res_path, res, [int(cv2.IMWRITE_PNG_STRATEGY), cv2.IMWRITE_PNG_STRATEGY_DEFAULT ])
-    else:
-        print("WRONG OUTPUT TYPE", suffix)
+    cv2.imwrite(res_path, res, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
 
 if __name__ == '__main__':
-    # print(file_extension("a.txt.jpg.png"))
     main()
